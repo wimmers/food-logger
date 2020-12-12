40ce31cbec8d49c04a0115aab661b4109a621807
Simon Wimmer <wimmers@in.tum.de>
2020-12-12 23:02:29 +0100

    refac(Util) add source for useAsyncReference

--- src/Util.tsx
+++ src/Util.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 
+// This code is from https://www.robinwieruch.de/react-function-component
 export function useAsyncReference<T>(value: T):
     [React.MutableRefObject<T>, (newState: T) => void] {
     const ref = useRef(value);
